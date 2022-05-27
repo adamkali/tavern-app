9117e6bc7c32de6dfe05d66dd46dc7b22140bf88
Adam Kalinowski <Adam>
2022-05-27 09:20:58 -0400

    All

---
+++ components/colorthemes.tsx
@@ -0,0 +1,11 @@
+export const PaperPen = {
+    dark: true,
+    colors: {
+        background: '#8C5940',
+        primary: '#F9E8B4',
+        text: '#0B032D',
+        border: '#F9E8B4',
+        notification: '#386FA4',
+        card: '#F9E8B4'
+    }
+};
