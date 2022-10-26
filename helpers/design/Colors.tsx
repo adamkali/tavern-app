d8300d043b5f0c864da53d3509dda24781e6c877
Adam Kalinowski <Adam>
2022-10-26 17:21:48 -0400

    Add Better Colors

--- helpers/design/Colors.tsx
+++ helpers/design/Colors.tsx
@@ -1,60 +1,85 @@
-export const DarkColors = {
-    // Foreground colors
+import './colors.css';
+
+export const Light = {
+    name: 'light',
+    foreground: {
+        primary: '#353535',
+        secondary: '#83827D',
+        tertiary: '#000000',
+    },
+    background: {
+        normal: '#FFFEEF',
+        accent: '#F9FFDD',
+        dark: '#FFFCCC',
+    },
+};
+
+export const Dark = {
     name: 'dark',
     foreground: {
-        primary: '#2f4ebf',
-        secondary: '#d54882',
-        tertiary: '#02f0ba',
+        primary: '#FFFEEF',
+        secondary: '#F9FFDD',
+        tertiary: '#FFFCCC',
     },
-    // Background colors
     background: {
-        normal: '#001564',
+        normal: '#353535',
+        accent: '#83827D',
+        dark: '#000000',
+    },
+};
+
+export const Royal = {
+    name: 'royal',
+    foreground: {
+        primary: '#6B5600',
+        secondary: '#B79300',
+        tertiary: '#B77500',
+    },
+    background: {
+        normal: '#FFD21A',
+        accent: '#FFF81A',
+        dark: '#FFAC1A',
+    },
+};
+
+export const Vaporwave = {
+    name: 'vaporwave',
+    foreground: {
+        primary: '#aefdf2',
+        secondary: '#5159ff',
+        tertiary: '#FF00FF',
+    },
+    background: {
+        normal: '#001e8c',
         accent: '#5204b0',
         dark: '#0b031e',
     },
 };
 
-export const LightColors = {
-    name: 'light',
+export const Blue = {
+    name: 'blue',
     foreground: {
-        primary: '#0b031e',
-        secondary: '#5204b0',
-        tertiary: '#001e8c',
+        primary: '#9695A0',
+        secondary: '#0B0091',
+        tertiary: '#0B0277',
     },
-    // Background colors
     background: {
-        normal: '#aefdf2',
-        accent: '#9060ab',
-        dark: '#5159ff',
+        normal: '#3E369D',
+        accent: '#54309A',
+        dark: '#304F97',
     },
 };
 
-export const VaporwaveColors = {
-    name: 'vaporwave',
+export const Fruit = {
+    name: 'fruit',
     foreground: {
-        primary: '#aefdf2',
-        secondary: '#9060ab',
-        tertiary: '#5159ff',
+        primary: '#FF0000',
+        secondary: '#6CEE00',
+        tertiary: '#00CD75',
     },
-    // Background colors
     background: {
-        normal: '#0b031e',
-        accent: '#5204b0',
-        dark: '#001e8c',
-    },
-};
-
-export const HelloKittyColors = {
-    name: 'hellokitty',
-    foreground: {
-        primary: '#E80D2B',
-        secondary: '#ECCD1F',
-        tertiary: '#ECCD1F',
-    },
-    // Background colors
-    background: {
-        normal: '#FFFFFF',
-        accent: '#41B5E2',
-        dark: '#E80D2B',
+        normal: '#6C0000',
+        accent: '#2B6000',
+        dark: '#004A2A',
     },
 };
